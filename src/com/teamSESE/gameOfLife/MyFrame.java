19dbcb8ede1b3e0089562213c180f5196a85ffd0
Ethan Archer <ethanarcher1@gmail.com>
2014-02-18 11:43:20 +0000

    MyFrame modified to add drop down

--- src/com/teamSESE/gameOfLife/MyFrame.java
+++ src/com/teamSESE/gameOfLife/MyFrame.java
@@ -1,12 +1,17 @@
 package com.teamSESE.gameOfLife;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class MyFrame extends JFrame 
 {
 	
 	JButton start;
+	String items[] = {"-","1","2","3","4"};
+	JComboBox combo = new JComboBox(items);
+	JLabel numOfP = new JLabel("Select Number Of Players");
+	JTextField players = new JTextField(20);
 	
 	public MyFrame(int x, int y, String title, Color col)
   {
@@ -20,7 +25,38 @@ public class MyFrame extends JFrame
       
       start = new JButton();
       start.setText("Start");
+      c.add(numOfP);
+      c.add(combo);
       c.add(start);
+      c.add(players);
+      
+      combo.addItemListener(new ItemListener(){
+        public void itemStateChanged(ItemEvent ie){
+     
+       if(ie.getStateChange() == ItemEvent.SELECTED){
+        String str = (String)combo.getSelectedItem();
+        
+        
+        
+        if(str=="1"){
+        	players.setText("1 Player selected");}
+      
+        if(str=="2"){
+        	players.setText("2 Players selected");}
+      
+        if(str=="3"){
+        	players.setText("3 Players selected");}
+      
+        if(str=="4"){
+        	players.setText("4 Players selected");}
+       }
+
+       }
+        
+      }
+      
+      );
+      
 
       setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
       setVisible(true);
