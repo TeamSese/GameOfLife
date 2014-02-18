f4fb0c620c3448f6384a2a5b02dacb705383e01f
Ethan Archer <ethanarcher1@gmail.com>
2014-02-18 10:49:14 +0000

    Initial test class has been removed and main and myframe have been added

---
+++ src/com/teamSESE/gameOfLife/Main.java
@@ -0,0 +1,13 @@
+package com.teamSESE.gameOfLife;
+
+import java.awt.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MyFrame f1 = new MyFrame(100,100, "Game Of Life", Color.lightGray);
+
+	}
+
+}
