73f843d2e153feafd010a3db6d51a5e10afb3876
Ethan Archer <ethanarcher1@gmail.com>
2014-02-20 20:13:17 +0000

    Commented the code in MyFrame

--- src/com/teamSESE/gameOfLife/MyFrame.java
+++ src/com/teamSESE/gameOfLife/MyFrame.java
@@ -6,47 +6,44 @@ import javax.swing.*;
 
 public class MyFrame extends JFrame 
 {
-	//blah blah blah test test test
-	//TEST FOR MAEVEVEVEVEVEVEVEE
-	//Maeve testing
-	JButton start;
-	String items[] = {"-","1","2","3","4","5","6"};
-	JComboBox combo = new JComboBox(items);
-	JLabel numOfP = new JLabel("Select Number Of Players");
-	JTextField players = new JTextField(20);
+	JButton startButton;																											//start button creation
+	String items[] = {"-","1","2","3","4","5","6"};											// options of number of players
+	JComboBox combo = new JComboBox(items);															// this is the drag down box
+	JLabel numOfP = new JLabel("Select Number Of Players");							// label of instructions
+	JTextField players = new JTextField(20);														// text box shows how many players are selected
 	
 	public MyFrame(String title, Color col)
   {
-      setTitle(title);
-      setSize(500, 500);
-      setLocationRelativeTo(null);
+      setTitle(title);																								//title is set in main method
+      setSize(500, 500);																						// this may need changed but looks ok 
+      setLocationRelativeTo(null);																	// sets the location as the centre of the screen
 
-      Container c = getContentPane();
-      c.setBackground(col);
-      getContentPane().setLayout(null);
+      Container c = getContentPane();																	//container to hold the different panels
+      c.setBackground(col);																						// col from main
+      getContentPane().setLayout(null);																//no layout selected so we can place anywhere
       
-      JPanel welcome = new JPanel();
-      welcome.setBounds(155, 5, 190, 26);
-      JLabel welcomeMessage = new JLabel ("Welcome to the Game of Life");
-      welcome.add(welcomeMessage);
-      c.add(welcome);
+      JPanel welcome = new JPanel();																			//wecome to the game message to display at top of box
+      welcome.setBounds(155, 5, 190, 26);																	// location
+      JLabel welcomeMessage = new JLabel ("Welcome to the Game of Life"); //message
+      welcome.add(welcomeMessage);																				//add to panel
+      c.add(welcome);																											// add to container
       
-      JPanel playerPanel = new JPanel(); 
-      playerPanel.setBounds(92, 225, 316, 27);
-      playerPanel.setLayout(new GridLayout(1,2));
-      c.add(playerPanel);
-      playerPanel.add(numOfP);
-      playerPanel.add(combo);
+      JPanel playerPanel = new JPanel(); 																// new panel for players
+      playerPanel.setBounds(92, 225, 316, 27);												// location
+      playerPanel.setLayout(new GridLayout(1,2));											//layout as grid
+      c.add(playerPanel);																							//add to container
+      playerPanel.add(numOfP);																				//label to tell to select num of players
+      playerPanel.add(combo);																					// drop down menu
       
-      combo.addItemListener(new ItemListener(){
+      combo.addItemListener(new ItemListener(){											//this is to set the number of players to be displayed
         public void itemStateChanged(ItemEvent ie){
      
-       if(ie.getStateChange() == ItemEvent.SELECTED){
-        String str = (String)combo.getSelectedItem();
+       if(ie.getStateChange() == ItemEvent.SELECTED){							
+        String str = (String)combo.getSelectedItem();								// if the number changes then change the number of player message
         
         
         
-        if(str=="1"){
+        if(str=="1"){																								// self explanatory
         	players.setText("1 Player selected");}
       
         if(str=="2"){
@@ -63,31 +60,30 @@ public class MyFrame extends JFrame
         
         if(str=="6"){
         	players.setText("6 Players selected");}
-       }
+       }																							// closes if statements
 
-       }
+       }																								//closes method
         
-      }
+      }																								//closes action listener
       
-      );
+      );																										//closes action listener
       
-      JPanel startPanel = new JPanel(); 
-      startPanel.setBounds(123, 264, 254, 58);
-      startPanel.setLayout(new GridLayout(2,1));
-      c.add(startPanel);
-      startPanel.add(players);
-      start = new JButton();
-      start.setBounds(123, 348, 254, 29);
-      getContentPane().add(start);
-      start.setText("Start");
+      JPanel startPanel = new JPanel(); 									//start panel for start button
+      startPanel.setBounds(123, 264, 254, 70);						//location 
+      startPanel.setLayout(new GridLayout(2,1));					// using a grid layout
+      startPanel.add(players);														// text box says number of payers
+      startButton = new JButton();												// actual start button
+      startPanel.add(startButton);												// button added
+      startButton.setText("Start");											// button has the label start
+      c.add(startPanel);																	// added to container
       
-      JButton btnHelp = new JButton("Help");
-      btnHelp.setBounds(191, 399, 117, 29);
-      getContentPane().add(btnHelp);
+      JButton btnHelp = new JButton("Help");							//help button created with label help
+      btnHelp.setBounds(191, 399, 117, 29);								//set a location
+      getContentPane().add(btnHelp);											// added to container
       
 
-      setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-      setVisible(true);
+      setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);			//close when closed
+      setVisible(true);																			// takes off invisability cloak
 
   } // MyFrame constructor
-}
+}  // end of MyFrame class
