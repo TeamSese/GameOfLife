5f2fb15dc02be6a7a0dce256cd6d32e365588eab
Ethan Archer <ethanarcher1@gmail.com>
2014-05-01 10:26:22 +0100

    House class updated

--- src/com/teamSESE/gameOfLife/House.java
+++ src/com/teamSESE/gameOfLife/House.java
@@ -2,6 +2,14 @@ package com.teamSESE.gameOfLife;
 
 public class House {
 	
+	String houseType;
+	int houseValue;
+	
+	public House (String houseType, int houseValue)
+	{
+		this.houseType = houseType;
+		this.houseValue = houseValue;
+	}
 	
 
 }
