2d624dd14f0e06926987a409d476ad9d15fec2ac
Stephen Donaldson <stephen.donaldson2@gmail.com>
2014-05-01 13:00:23 +0100

    changed main

--- src/com/teamSESE/gameOfLife/Main.java
+++ src/com/teamSESE/gameOfLife/Main.java
@@ -6,10 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//GameMechanics.setUpCareers();
-		//GameMechanics.setUpCourses();
+		GameMechanics.setUpCourses();
 		MyFrame f1 = new MyFrame("Game Of Life", Color.lightGray);
-		//DebugGUI g1 = new DebugGUI();
+		DebugGUI g1 = new DebugGUI();
 
 	}
 
