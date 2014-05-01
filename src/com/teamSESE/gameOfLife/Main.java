f7c2c9c75bf063cf4fafd05c557bc41354d4067b
Stephen Donaldson <stephen.donaldson2@gmail.com>
2014-05-01 22:19:36 +0100

    Tidy up main

--- src/com/teamSESE/gameOfLife/Main.java
+++ src/com/teamSESE/gameOfLife/Main.java
@@ -6,19 +6,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		
 		GameMechanics.setUpCourses();
-<<<<<<< HEAD
 		GameMechanics.setUpHouses();
 		MyFrame f1 = new MyFrame("Game Of Life", Color.lightGray);	//Selects number of Players
 		
-		//DebugGUI g1 = new DebugGUI();
-		
-	
-=======
-		MyFrame f1 = new MyFrame("Game Of Life", Color.lightGray);
-		//DebugGUI g1 = new DebugGUI();
-
->>>>>>> ae98a2bb9dc31ca7a762b7107cf5e02eec0635f2
 	}
 
 }
