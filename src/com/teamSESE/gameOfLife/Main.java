ddb53bb1ebf54958613ff32f433f5ad8abde68af
Ethan Archer <ethanarcher1@gmail.com>
2014-05-02 16:50:50 +0100

    Recovered main, good one timmy

---
+++ src/com/teamSESE/gameOfLife/Main.java
@@ -0,0 +1,18 @@
+package com.teamSESE.gameOfLife;
+
+import java.awt.*;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		
+		
+		GameMechanics.setUpCourses();
+		GameMechanics.setUpHouses();
+		GameMechanics.setUpBoard();
+
+		MyFrame f1 = new MyFrame("Game Of Life", Color.lightGray);	//Selects number of Players	
+	}
+
+}
