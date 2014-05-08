83cd166407f07cf292ca1f7308549268d69bb749
Stephen Donaldson <stephen.donaldson2@gmail.com>
2014-05-08 08:47:20 +0100

    Sorted out duplicate code

--- src/com/teamSESE/gameOfLife/GameMechanics.java
+++ src/com/teamSESE/gameOfLife/GameMechanics.java
@@ -22,7 +22,6 @@ public class GameMechanics {
 	static String tileString[] = {
 		"Select Course",					//0
 		"Defered start - miss a turn!",		//1
-<<<<<<< HEAD
 		"Student loan day! Receive £1000.",				//2		GREEN
 		"Switch courses - miss a turn, pay £3000.",		//3
 		"Get lost on a night out - miss a turn.",	//4
@@ -76,68 +75,10 @@ public class GameMechanics {
 		"Invest in Bitcoin! Lose £200",				//52
 		"Grad party! Pay £200.",		//53
 		"Game OVER!",						//54
-=======
-		"Student loan day! Receive £1000.",				//2
-		"Switch courses - miss a turn, pay £3000.",		//3
-		"Get lost on a night out - miss a turn.",	//4
-		"'Borrow' money from another player...",						//5
-		"Buy a bike - pay £200.",					//6
-		"Win scholarship - receive £500",			//7
-		"Fail an exam - miss a turn",					//8
-		"STOP - move out of elms",						//9
-		"Student Loan day!",							//10
-		"House gets broken into - loose £200",							//11
-		"Go on placement year - Receive: £5000",						//12
-		"Become addicted to something  - miss a turn",					//13
-		"Bet against your friend --- ",									//14
-		"Find some money - Receive £200",								//15
-		"Leave Game of Life Coursework to Last Minute - Miss 2 turns!!",//16
-		"Pass exams - Get a life tolken",								//17
-		"Student Loan day!",											//18
-		"Asroid stike on new house  - Gain a life tolken",				//19
-		"Bet against your friend --- ",						//20
-		"Meet your future wife - loose £500",				//21
-		"Move House!",										//22
-		"Forget to return book - loose £30",				//23
-		"Student Loan day!",								//24
-		"Student Loan day!",								//2
-		"Don't like course - Switch!",						//3
-		"Get lost on a night out - Miss a turn",			//4
-		"Sue another player",								//5
-		"Buy a bike - loose £200",							//6
-		"Receive Scholarship - Receive £500",				//7
-		"Fail an exam - miss a turn",						//8
-		"STOP - Move out of elms",							//9
-		"Student Loan day!",								//10
-		"House gets broken into - loose £200",							//11
-		"Go on placement year - Receive: £5000",						//12
-		"Become addicted to something  - miss a turn",					//13
-		"Bet against your friend --- ",									//14
-		"Find some money - Receive £200",								//15
-		"Leave Game of Life Coursework to Last Minute - Miss 2 turns!!",//16
-		"Pass exams - Get a life tolken",								//17
-		"Student Loan day!",											//18
-		"Asroid stike on new house  - Gain a life tolken",				//19
-		"Bet against your friend --- ",						//20
-		"Meet your future wife - loose £500",				//21
-		"Move House!",										//22
-		"Forget to return book - loose £30",				//23
-		"Student Loan day!",								//24
-		"Student Loan day!",				//46
-		"Don't like course - Switch!",		//47
-		"Get lost on a night out - Miss a turn",	//48
-		"Sue another player",						//49
-		"Student Loan day!",				//50
-		"Don't like course - Switch!",		//51
-		"Get lost on a night out - Miss a turn",	//52
-		"Sue another player",						//53
-		"Game OVER!",						//53
->>>>>>> 35fdf77eb4b15ce9588fd0c32e9361f865c1d4bc
 		
 	};
 	static int tileProperties[] = {		//What each tile should do goes in here - in order!
 								//Tile Number
-<<<<<<< HEAD
 		Tile.SELECT_COURSE,3,4,			//0
 		Tile.MISS_TURN,2,4,				//1
 		Tile.GAIN_LOOSE_1000,1,4,		//2 	Green
@@ -147,27 +88,16 @@ public class GameMechanics {
 		Tile.LOOSE_200,1,2,				//6	
 		Tile.BORROW_100,1,1,			//7		Blue
 		Tile.MISS_TURN,1,0,				//8
-=======
-		Tile.SELECT_COURSE,3,4,		//0
-		Tile.GAIN_500,2,4,			//1
-		Tile.PAY_DAY,1,4,				//2
-		Tile.LOOSE_200,0,4,				//3
-		Tile.GAIN_500,0,3,				//4
-		Tile.GAIN_200,0,2,				//5
-		Tile.LOOSE_500,1,2,				//6
-		Tile.FINISH,1,1,		//7
-		Tile.GAIN_200,1,0,				//8
->>>>>>> 35fdf77eb4b15ce9588fd0c32e9361f865c1d4bc
 		Tile.GAIN_200,2,0,				//9
 		Tile.GAIN_LOOSE_1000,3,0,		//10	Green
 		Tile.LOOSE_200,4,0,				//11
 		Tile.GAIN_500,4,1,				//12
-		Tile.MISS_TURN,3,1,			//13
+		Tile.MISS_TURN,3,1,				//13
 		Tile.GAIN_200,3,2,				//14
-		Tile.SELECT_HOUSE,				//15	Red
+		Tile.SELECT_HOUSE,4,2,				//15	Red
 		Tile.MISS_TURN,5,2,				//16
 		Tile.BORROW_100,5,1,			//17	Blue
-		Tile.LOOSE_100,					//18
+		Tile.LOOSE_100,6,1,				//18
 		Tile.GAIN_LOOSE_1000,6,2,		//19	Green
 		Tile.LOOSE_500,6,3,				//20
 		Tile.LOOSE_500,5,3,				//21
@@ -203,7 +133,7 @@ public class GameMechanics {
 		Tile.LOOSE_500,1,5,				//51
 		Tile.LOOSE_200,2,5,				//52
 		Tile.LOOSE_200,3,5,				//53
-		Tile.FINISH,4,5,				//54
+		Tile.FINISH,4,5,					//54
 		
 		};
 	
@@ -238,7 +168,8 @@ public class GameMechanics {
 		for(int i = 0; i < tileProperties.length; i=i+3){			//This will create the new tiles as they are in the tileProperties array
 			System.out.println(i);
 			tileList.add(new Tile(tileProperties[i],tileProperties[i+1],tileProperties[i+2],tileString[j])); 			//and add them to the tileList ArrayList (the board)
-			System.out.println("Tile: " + j + " X: "+tileList.get(j).gridXPos +" Y: "+ tileList.get(j).gridYPos);
+			System.out.println("Tile: " + j + " X: "+tileList.get(j).gridXPos +" Y: " + tileList.get(j).gridYPos);
+			System.out.println(tileString[j]);
 			j++;
 		}
 	}
