a7a812d5c1c6b45dbf65eb8a9aa6f6969837ff7b
Ethan Archer <ethanarcher1@gmail.com>
2014-05-08 14:17:44 +0100

    - Wrote game instructions

--- src/com/teamSESE/gameOfLife/instructionsFrame.java
+++ src/com/teamSESE/gameOfLife/instructionsFrame.java
@@ -12,14 +12,17 @@ import java.awt.event.*;
 		JTextArea rules;
 		JButton ok;
 		String dLine = "\n" + "\n";
-		String theRules = "THE RULES FOR HOW TO PLAY SHOULD GO HERE";
+		String theRules = "GAME OF LIFE - QUB EDITION INSTRUCTIONS\n\n1. Players take turns to roll a dice by pressing the 'Roll Dice'\nbutton on the top right-hand side of the game panel. "
+				+ "The\nplayer's counter then moves the appropriate number of spaces\nand lands on a tile.\n2. Orange tiles represent the day-to-day activities and events\nthat a student might encounter at University. Blue tiles allow\na player to indefinitely 'borrow' a sum of "
+				+ "money from\nanother player. Green tiles represent income in the form of\nstudent loans (which much be paid back with 20% interest at\nthe end of the game) or a salary should the player manage\nto obtain a placement during the course of their studies at\nQUB. All players must "
+				+ "stop on the red tile where they\nmust choose a new house to live in once they move out\nof Elms Village accommodation.\n3. The winner is the player that accrues the most money\nand 'life tiles' by the time they reach the end of the game\nboard.";
 		
 		public instructionsFrame()
 		{
 			//Initialise the frame
 			setLocation(400, 75);
 			setTitle("Instructions");
-			setSize(500, 400);
+			setSize(450, 400);
 			Container c = getContentPane();
 			c.setLayout(new FlowLayout());
 			//Initialise the text area for the rules and the ok button for the bottom
@@ -28,6 +31,7 @@ import java.awt.event.*;
 			JButton ok = new JButton("OK");
 			//Initialise the JPanels and place the content onto them
 			text = new JPanel();
+			text.setSize(500, 300);
 			text.add(rules);
 			button = new JPanel();
 			button.add(ok);
